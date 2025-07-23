6adab55a5ff2000f60737a7a3580a2c6822856da
trungung <trung.ungtoan@asnet.com.vn>
2025-07-23 14:54:27 +0700

    Add learn-terraform-provider-versioning package (cleaned of license and .git)

---
+++ fundamental/learn-terraform-provider-versioning/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "random_pet" "petname" {
+  length    = 5
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "sample" {
+  bucket = random_pet.petname.id
+
+  tags = {
+    public_bucket = false
+  }
+}
---
+++ fundamental/learn-terraform-provider-versioning/terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  /* Uncomment this block to use Terraform Cloud for this tutorial
+  cloud {
+    organization = "organization-name"
+    workspaces {
+      name = "learn-terraform-provider-versioning"
+    }
+  }
+  */
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.5.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
