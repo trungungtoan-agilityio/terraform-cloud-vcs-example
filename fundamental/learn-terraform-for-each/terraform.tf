6d431e798ec18e176ee6e0702a2c9a1c24674dc7
trungung <trung.ungtoan@asnet.com.vn>
2025-07-23 10:47:00 +0700

    Add learn-terraform-for-each package with code, variables, outputs, and documentation

---
+++ fundamental/learn-terraform-for-each/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    workspaces {
+      name = "learn-terraform-for-each"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.22.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
